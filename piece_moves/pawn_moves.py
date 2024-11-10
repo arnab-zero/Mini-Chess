538c4e6ce46dc171551862c10d4d378cf4e73871
arnab-zero <darnabhere@gmail.com>
2024-11-11 02:16:13 +0600

    Made changes in moving pawn and queen pieces

--- piece_moves/pawn_moves.py
+++ piece_moves/pawn_moves.py
@@ -1,36 +1,46 @@
 from Moves import Move
 
+pins = []
+board = [
+            ["bR", "bB", "bK", "bQ", "bN"],
+            ["bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wB", "wK", "wQ", "wN"]
+        ]
+
 def getPawnMoves(self, r, c, moves):
     piecePinned = False
     pinDirection = ()
-    for i in range((len(self.pins) - 1), -1, -1):
-        if r == self.pins[i][0] and c == self.pins[i][1]:
+    for i in range((len(pins) - 1), -1, -1):
+        if r == pins[i][0] and c == pins[i][1]:
             piecePinned = True
-            pinDirection = (self.pins[i][2], self.pins[i][3])
-            self.pins.remove(self.pins[i])
+            pinDirection = (pins[i][2], pins[i][3])
+            pins.remove(pins[i])
             break
 
-    if self.whiteToMove:
-        if self.board[r - 1][c] == '--':
+    if whiteToMove:
+        if board[r - 1][c] == '--':
             if not piecePinned or pinDirection == (-1, 0):
-                moves.append(Move((r, c), (r - 1, c), self.board))
+                moves.append(Move((r, c), (r - 1, c), board))
         if c - 1 >= 0:
             if not piecePinned or pinDirection == (-1, -1):
-                if self.board[r - 1][c - 1][0] == 'b':
-                    moves.append(Move((r, c), (r - 1, c - 1), self.board))
-        if c + 1 < len(self.board[r]):
+                if board[r - 1][c - 1][0] == 'b':
+                    moves.append(Move((r, c), (r - 1, c - 1), board))
+        if c + 1 < len(board[r]):
             if not piecePinned or pinDirection == (-1, 1):
-                if self.board[r - 1][c + 1][0] == 'b':
-                    moves.append(Move((r, c), (r - 1, c + 1), self.board))
+                if board[r - 1][c + 1][0] == 'b':
+                    moves.append(Move((r, c), (r - 1, c + 1), board))
     else:
-        if self.board[r + 1][c] == '--':
+        if board[r + 1][c] == '--':
             if not piecePinned or pinDirection == (1, 0):
-                moves.append(Move((r, c), (r + 1, c), self.board))
+                moves.append(Move((r, c), (r + 1, c), board))
         if c - 1 >= 0:
             if not piecePinned or pinDirection == (1, -1):
-                if self.board[r + 1][c - 1][0] == 'w':
-                    moves.append(Move((r, c), (r + 1, c - 1), self.board))
-        if c + 1 < len(self.board[r]):
+                if board[r + 1][c - 1][0] == 'w':
+                    moves.append(Move((r, c), (r + 1, c - 1), board))
+        if c + 1 < len(board[r]):
             if not piecePinned or pinDirection == (1, 1):
-                if self.board[r + 1][c + 1][0] == 'w':
-                    moves.append(Move((r, c), (r + 1, c + 1), self.board))
+                if board[r + 1][c + 1][0] == 'w':
+                    moves.append(Move((r, c), (r + 1, c + 1), board))
--- piece_moves/queen_moves.py
+++ piece_moves/queen_moves.py
@@ -1,5 +1,14 @@
 from rook_moves import getRookMoves
 
+pins = []
+board = [
+            ["bR", "bB", "bK", "bQ", "bN"],
+            ["bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wB", "wK", "wQ", "wN"]
+        ]
 
 def getQueenMoves(self, r, c, moves):
     getRookMoves(r, c, moves)
